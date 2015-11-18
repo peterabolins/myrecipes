f8f3643fa4f35948085c35fa239270460970a01b
Peter Abolins <peter@peterabolins.com>
2015-11-18 19:00:24 +0000

    Added gems fog and net-ssh

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-Rails::Initializer.run do |config|
-  config.gem 'net-ssh', :lib => 'net/ssh'
-end
-
 # Initialize the Rails application.
 Rails.application.initialize!
 
