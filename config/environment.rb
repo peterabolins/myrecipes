f7f6730b9f8aec226e40225523fc05001a8e423b
Peter Abolins <peter@peterabolins.com>
2015-11-18 18:54:59 +0000

    Added workaround for net/ssh gem - more

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,11 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Rails::Initializer.run do |config|
+  config.gem 'net-ssh', :lib => 'net/ssh'
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
 require 'carrierwave/orm/activerecord'
-
-config.gem 'net-ssh', :lib => "net/ssh"
